e3138815bf6ceb69e526cfebc64a2f9068335219
fullstack-overkill <christophermms.ctt@gmail.com>
2021-01-31 23:25:38 -0300

    🎨 Home update

--- src/views/public/home/index.js
+++ src/views/public/home/index.js
@@ -1,5 +1,25 @@
-import './style.scss'
+import "./style.scss";
+import UiAvatar from "../../../components/ui/avatar";
 
-export default  () => {
-    return <h1>Home!</h1>;
-}
+import AppHeader from "../../../components/app/header";
+import AppComicsList from "../../../components/app/comicsList";
+
+export default () => {
+  return (
+    <div className="body">
+      <div className="coll-1">
+        <AppHeader />
+        <main>
+          <section className="row-1">S.H.I.E.L.D</section>
+          <section className="row-2">
+            Sua Biblioteca de Quadrinhos Online
+          </section>
+          <section className="row-3">
+            <AppComicsList />
+          </section>
+        </main>
+      </div>
+      <div className="coll-2"></div>
+    </div>
+  );
+};
