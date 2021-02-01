fb5834df0ef7b9d2abad81d9c27e1d8585779145
fullstack-overkill <christophermms.ctt@gmail.com>
2021-01-31 23:25:24 -0300

    🎨 Initial avatar component

---
+++ src/components/ui/avatar/index.js
@@ -0,0 +1,86 @@
+import "./style.scss";
+
+export default () => {
+  return (
+    <div className="ui-avatar">
+      <div class="wrapper">
+        <div class="logo">
+          <img
+            src="http://www.clipartroo.com/images/3/exciting-word-clipart-3646.png"
+            alt=""
+          />
+        </div>
+        <div class="wolverine">
+          <div class="wolverine-body">
+            <div class="wolverine-torso">
+              <div class="peck left">
+                <div class="wolverine-arm left">
+                  <div class="forearm"></div>
+                  <div class="fist">
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="wolverine-claws">
+                      <div class="wolverine-claw"></div>
+                      <div class="wolverine-claw"></div>
+                      <div class="wolverine-claw"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="shoulderpad"></div>
+              </div>
+              <div class="peck right">
+                <div class="wolverine-arm right">
+                  <div class="forearm"></div>
+                  <div class="fist">
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="finger"></div>
+                    <div class="wolverine-claws">
+                      <div class="wolverine-claw"></div>
+                      <div class="wolverine-claw"></div>
+                      <div class="wolverine-claw"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="shoulderpad"></div>
+              </div>
+            </div>
+            <div class="wolverine-head">
+              <div class="cowl-left">
+                <div class="eye"></div>
+              </div>
+              <div class="cowl-right">
+                <div class="eye"></div>
+              </div>
+              <div class="face">
+                <div class="mouth">
+                  <div class="tongue"></div>
+                  <div class="teeth"></div>
+                </div>
+              </div>
+            </div>
+            <div class="wolverine-leg right">
+              <div class="boot">
+                <div class="chevron"></div>
+              </div>
+            </div>
+            <div class="wolverine-crotch"></div>
+            <div class="wolverine-belt">
+              <div class="buckle"></div>
+            </div>
+            <div class="wolverine-leg left">
+              <div class="boot">
+                <div class="chevron"></div>
+              </div>
+            </div>
+          </div>
+          <div class="snikt left">Snikt!</div>
+          <div class="snikt right">Snikt!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
