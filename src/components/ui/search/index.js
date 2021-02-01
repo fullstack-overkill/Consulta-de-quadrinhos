e69122fe3487e35679862d35763b821d4c764534
fullstack-overkill <christophermms.ctt@gmail.com>
2021-01-31 23:24:29 -0300

    🎨 Initial search component

---
+++ src/components/ui/search/index.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+export default () => {
+  return (
+    <>
+      <section className="ui-search" role="search">
+        <span class="material-icons">search</span>
+      </section>
+    </>
+  );
+};
