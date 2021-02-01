0a87ad137ff54504c7c25a0bf4edd801a93e1e0a
fullstack-overkill <christophermms.ctt@gmail.com>
2021-01-31 23:23:55 -0300

    🎨 App logo component

---
+++ src/components/app/logo/index.js
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+export default () => {
+  return (
+    <div className="app-logo">
+      <span className="line-1">
+        <span className="ball"></span>
+        <span className="ball"></span>
+      </span>
+      <span className="line-2">
+        <span className="ball"></span>
+        <span className="ball ball-allt"></span>
+      </span>
+    </div>
+  );
+};
